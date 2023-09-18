e86e63ea8d680fe410d5a7f87b4d7266eb4efcc8
Alcibiades Acosta <alcibiadesacosta99@gmail.com>
2023-09-18 10:52:39 -0500

    modifique mi segundo commit

--- MODULO 2/clase 5/microdesafio1.js
+++ MODULO 2/clase 5/microdesafio1.js
@@ -3,9 +3,9 @@ let precio = { compacto: 14000, mediano: 17000, camioneta: 28000 };
 let diasDeAlquiler = 5;
 let sillaParaBebe = 1200;
 function alquilerDeUnVehiculo(tipoDeVehiculo, diasDeAlquiler, sillaParaBebeDeseada) {
-    const costoVehiculo = precio[tipoDeVehiculo];
-    const costoSilla = sillaParaBebeDeseada ? sillaParaBebe : 0;
-    const montoTotal = costoVehiculo * diasDeAlquiler + costoSilla;
+    let costoVehiculo = precio[tipoDeVehiculo];
+    let costoSilla = sillaParaBebeDeseada ? sillaParaBebe : 0;
+    let montoTotal = costoVehiculo * diasDeAlquiler + costoSilla;
     console.log(`Estimado cliente: en base al tipo de vehículo ${tipoDeVehiculo} alquilado, considerando los ${diasDeAlquiler} días utilizados.${sillaParaBebeDeseada ? ` Además, se incluye el costo de las sillas para bebé que solicitó, que es de $${sillaParaBebe}.` : ''} El monto total a pagar es de $${montoTotal}.`);
 }
 alquilerDeUnVehiculo(tipoDeVehiculo, diasDeAlquiler, true);
