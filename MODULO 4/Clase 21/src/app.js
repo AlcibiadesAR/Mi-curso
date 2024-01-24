9b7ebbd759cd22e7f0a189072a6de17180cce95c
Alcibiades <alcibiadesacosta99@gmail.com>
2024-01-24 10:17:13 -0500

    instale dependencias

--- MODULO 4/Clase 21/src/app.js
+++ MODULO 4/Clase 21/src/app.js
@@ -6,9 +6,7 @@ const app = express();
 const publicPath = path.resolve(__dirname, '../public') //corregí lo que tenías: "..", ".." por solo "../public"
 app.use(express.static(publicPath))
 
-app.listen(3002, () => {
-    console.log('servidor en el puerto 3002')
-})
+app.listen(3012, () => {})
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html')) // para acceder al home.html 
